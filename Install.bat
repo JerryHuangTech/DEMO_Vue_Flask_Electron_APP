159c7438ab411a7a828f53fa9b0c458f403898eb
pekora-tech <123544007+pekora-tech@users.noreply.github.com>
2024-02-02 11:05:25 +0800

    上傳基礎程式

---
+++ Install.bat
@@ -0,0 +1,4 @@
+
+python -m venv venv
+venv\Scripts\activate
+pip install influxdb-client cryptography opcua pyinstaller flasgger Flask-WTF flake8 black isort mypy flask python-dotenv requests PyYAML
